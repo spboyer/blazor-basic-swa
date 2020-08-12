fd26d417bddc8a53912fccb8b1dedc109f1d2266
Shayne Boyer <spboyer@live.com>
2020-08-12 13:39:46 -0400

    Add project files.

---
+++ WeatherForecastApi/WeatherForecastFunction.cs
@@ -0,0 +1,44 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System.Linq;
+
+namespace Api
+{
+    public static class WeatherForecastFunction
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        [FunctionName("WeatherForecast")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            var rng = new Random();
+            return new OkObjectResult(Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray());
+        }
+
+        public class WeatherForecast
+        {
+            public DateTime Date { get; set; }
+            public int TemperatureC { get; set; }
+            public string Summary { get; set; }
+            public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+        }
+    }
+}
